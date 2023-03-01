fcf3722e1788b0f317ff155fa018cd964e5f9a5b
Ali Haider <110288169+Alihaider7707@users.noreply.github.com>
2023-03-01 14:11:54 +0500

    alihaider

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { Switch ,Route, Redirect} from "react-router-dom";
+import Home from "./Home";
 
 function App() {
   return (
 <>
-
+<Switch>
+  <Route exact path="/" component={Home}/>
+  <Redirect to= '/'/>
+</Switch>
 </>
   );
 }
