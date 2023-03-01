cc887811e1c01f18c75742cb10a82f1bcae2a463
Ali Haider <110288169+Alihaider7707@users.noreply.github.com>
2023-03-01 16:34:13 +0500

    alihaider

--- src/Pages/Banner1.jsx
+++ src/Pages/Banner1.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from "react";
 import "../Pages/Banner1.css";
 
 const Banner1 = () => {
   return (
-  <>
-  <div className="banner1-text">
-    <h1>Plane Your Trip with Grapeslab</h1>
-  </div>
-  </>
-  )
-}
+    <>
+      <div className="banner1-text">
+        <h1>Plane Your Trip with Grapeslab</h1>
+        <p>
+          Throughout our lives, people have thought that because we keep
+          traveling and searching for new things to do, we are focused on
+          travel.
+        </p>
+      </div>
+    </>
+  );
+};
 
-export default Banner1
+export default Banner1;
