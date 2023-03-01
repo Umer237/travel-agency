7630ea6840a4de24d746c6fcb4288805c80193d6
Ali Haider <110288169+Alihaider7707@users.noreply.github.com>
2023-03-01 16:23:59 +0500

    alihaider

--- src/Pages/Banner1.jsx
+++ src/Pages/Banner1.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import './Banner.css'
+import "../Pages/Banner1.css";
 
 const Banner1 = () => {
   return (
   <>
-  
+  <div className="banner1-text">
+    <h1>Plane Your<br></br>Trip with<br></br>Grapeslab</h1>
+  </div>
   </>
   )
 }
