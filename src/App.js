f54be950160a36b161ed4b7dbdea36342a30e6e4
Ali Haider <110288169+Alihaider7707@users.noreply.github.com>
2023-03-01 15:25:34 +0500

    alihader

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "./Pages/Home";
 import Header from "./Pages/Header";
+import Footer from "./Pages/Footer"
 
-import { Route, Switch } from "react-router-dom";
 function App() {
   return (
 <>
