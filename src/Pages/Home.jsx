2bda276f01fbcb33d4281778172838150aa43e30
Ali Haider <110288169+Alihaider7707@users.noreply.github.com>
2023-03-03 17:00:46 +0500

    alihaider

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Banner from "./Banner";
 import Section2 from "./Section2";
 import Section3 from "./Section3";
 import Section5 from "./Section5";
+import Section7 from "./Section7";
 
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
  <Banner/>
  <Section2/>
  <Section3/>
+ <Section7/>
  <Section5/>
     </>
   );
