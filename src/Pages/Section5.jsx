a5cc7d71e6e669b5fcac36cf466cc20d63bb280b
Ali Haider <110288169+Alihaider7707@users.noreply.github.com>
2023-03-03 18:03:54 +0500

    alihaider

--- src/Pages/Section5.jsx
+++ src/Pages/Section5.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Section5.css";
 import camera from "../images/camera.jpg";
+import sunrise from "../images/sunrise.jpg";
 
 const Section5 = () => {
   return (
     <>
       <div className="section5-bg">
+        <div className="section5-background">
           <div className="section5-text">
             <h2>A Haven On The Earth<br></br>For You</h2>
           </div>
@@ -14,7 +16,7 @@ const Section5 = () => {
             <img src={camera} alt="" />
           </div>
           <div className="section5-para">
-            <p>
+            <p style={{paddingBottom:"40px"}}>
               There are many variations of passages of Lorem Ipsum available,
               but the majority have suffered alteration in some form, by
               injected humour, or randomised words which don't look even
@@ -25,6 +27,10 @@ const Section5 = () => {
               but the majority have suffered alteration.
             </p>
           </div>
+          <div className="section5-img">
+          <img src={sunrise} alt="" />
+          </div>
+        </div>
         </div>
       </div>
     </>
